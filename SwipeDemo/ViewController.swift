28902e14ee167c71635c6c344da5002cd99e38f5
vale <changweitu@gmail.com>
2015-07-01 09:17:08 +0800

    Initial Commit

---
+++ SwipeDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SwipeDemo
+//
+//  Created by vale on 7/1/15.
+//  Copyright (c) 2015 changweitu@gmail.com. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
